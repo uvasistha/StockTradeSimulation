8f17e386238b1f1078c525e20744a79962db80e0
vashishthamishra <utkarsh.mishra30@gmail.com>
2020-06-18 03:27:05 +0530

    Added Service Properties.
    Signed-off-by: vashishthamishra <utkarsh.mishra30@gmail.com>

--- StockTradeSimluation/stock-real-time/src/main/java/com/stockrealtime/stockrealtime/StockRealTimeApplication.java
+++ StockTradeSimluation/stock-real-time/src/main/java/com/stockrealtime/stockrealtime/StockRealTimeApplication.java
@@ -2,7 +2,9 @@ package com.stockrealtime.stockrealtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(Config.class)
 @SpringBootApplication
 public class StockRealTimeApplication {
 
