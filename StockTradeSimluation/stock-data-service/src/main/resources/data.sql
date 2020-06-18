8cecc79e3bd059848a4b0c4f10130463f305e360
vashishthamishra <utkarsh.mishra30@gmail.com>
2020-06-19 00:41:23 +0530

    Added DataService Schema.
    Signed-off-by: vashishthamishra <utkarsh.mishra30@gmail.com>

---
+++ StockTradeSimluation/stock-data-service/src/main/resources/data.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS stock;
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE stock (
+  id VARCHAR(250) PRIMARY KEY,
+  open_val VARCHAR(250)  NULL,
+  high VARCHAR(250)  NULL,
+  low VARCHAR(250)  NULL,
+  price VARCHAR(250)  NULL,
+  volume VARCHAR(250)  NULL,
+  latest_trading_day VARCHAR(250)  NULL,
+  previous_close VARCHAR(250)  NULL,
+  change VARCHAR(250)  NULL,
+  change_percent VARCHAR(250)  NULL
+);
+
+CREATE TABLE user (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  stock_list VARCHAR(250)  NULL,
+  balance VARCHAR(250)  NULL,
+  profit VARCHAR(250)  NULL,
+  no_of_stock VARCHAR(250)  NULL,
+  name VARCHAR(250)  NULL,
+  email VARCHAR(250)  NULL,
+  mobile VARCHAR(250)  NULL,
+  password VARCHAR(250)  NULL,
+  premium_customer VARCHAR(250)  NULL
+);
+
