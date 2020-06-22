781c82ddd7cbba79efde3dd423743cf782abb243
vashishthamishra <utkarsh.mishra30@gmail.com>
2020-06-22 19:56:27 +0530

    Update as per UTC.
    Signed-off-by: vashishthamishra <utkarsh.mishra30@gmail.com>

--- StockTradeSimluation/stock-data-service/src/main/java/com/stockdataservice/Interface/Rest/ExternalHandler.java
+++ StockTradeSimluation/stock-data-service/src/main/java/com/stockdataservice/Interface/Rest/ExternalHandler.java
@@ -4,12 +4,16 @@ import com.stockdataservice.Interface.External.StockRealTime;
 import com.stockdataservice.Interface.External.model.StockExternal;
 import com.stockdataservice.Interface.Rest.Model.Stock;
 import com.stockdataservice.service.DataService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalTime;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 @Component
 public class ExternalHandler {
 
@@ -77,23 +81,33 @@ public class ExternalHandler {
 
     //schedule for every one hour to update
     @Scheduled(initialDelay = 1000*60*60, fixedDelay = 1000*60*60)
-    public void fetchOldStock(){
-        Iterable<com.stockdataservice.domain.Stock> allStock= dataService.getAllStock();
-        AtomicInteger atomicInteger = new AtomicInteger(0);
-        allStock.forEach(stock -> {
-            if(atomicInteger.get()==5){
-                atomicInteger.set(0);
-                try {
-                    Thread.sleep(1000*60*5);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+    public void fetchOldStock() {
+        LocalTime time = LocalTime.now();
+        //International Market Update
+        if (time.getHour() > 0 && time.getHour() < 21) {
+            Iterable<com.stockdataservice.domain.Stock> allStock = dataService.getAllStock();
+            AtomicInteger atomicInteger = new AtomicInteger(0);
+            allStock.forEach(stock -> {
+                if (atomicInteger.get() == 5) {
+                    atomicInteger.set(0);
+                    try {
+                        Thread.sleep(1000 * 60 * 5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-            StockExternal stockExternal = stockRealTime.getStockData(stock.getSymbol());
-            atomicInteger.set(atomicInteger.get()+1);
-            if(stockExternal != null){
-                updateStock(stockExternal);
-            }
-        });
+                StockExternal stockExternal = stockRealTime.getStockData(stock.getSymbol());
+                atomicInteger.set(atomicInteger.get() + 1);
+                if (stockExternal != null) {
+                    updateStock(stockExternal);
+                }
+            });
+        }
+    }
+
+    @PostConstruct
+    void info(){
+        LocalTime localTime = LocalTime.now();
+        log.info("External Handler Begins @ " + localTime.toString());
     }
 }
