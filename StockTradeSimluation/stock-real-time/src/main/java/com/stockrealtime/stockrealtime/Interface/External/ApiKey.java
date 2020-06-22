c36a853b9cade0f8599719c6b9d1223873e11913
vashishthamishra <utkarsh.mishra30@gmail.com>
2020-06-22 19:55:37 +0530

    Added Support for Multiple Keys.
    Signed-off-by: vashishthamishra <utkarsh.mishra30@gmail.com>

--- StockTradeSimluation/stock-real-time/src/main/java/com/stockrealtime/stockrealtime/Interface/External/ApiKey.java
+++ StockTradeSimluation/stock-real-time/src/main/java/com/stockrealtime/stockrealtime/Interface/External/ApiKey.java
@@ -2,10 +2,27 @@ package com.stockrealtime.stockrealtime.Interface.External;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiKey {
-    @JsonProperty("key")
-    String key;
+    @JsonProperty("keygen")
+    List<Key> keygen;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Key{
+        @JsonProperty("key")
+        String key;
+    }
 }
--- StockTradeSimluation/stock-real-time/src/main/java/com/stockrealtime/stockrealtime/Interface/External/StockFetcher.java
+++ StockTradeSimluation/stock-real-time/src/main/java/com/stockrealtime/stockrealtime/Interface/External/StockFetcher.java
@@ -9,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 import java.io.InputStream;
+import java.util.List;
 
 
 @Slf4j
@@ -19,31 +20,48 @@ public class StockFetcher {
     @Autowired
     ObjectMapper objectMapper;
     private String key = null;
+    private int index = 0;
+    private int size = 1;
+    List<ApiKey.Key> keys;
 
 
     @PostConstruct
     public void readKey(){
-        String data = null;
+        List<ApiKey.Key> data = null;
         try {
             InputStream is = ApiKey.class.getResourceAsStream("/apikey");
             ApiKey apiKey = objectMapper.readValue(is,ApiKey.class);
-            data = apiKey.getKey();
+            data = apiKey.getKeygen();
         } catch (Exception e){
-            log.info("Couldn't Read Key");
+            log.info("Couldn't Read Keys");
             e.printStackTrace();
         }
-        log.info("key" + data);
-        this.key=data;
+        log.info("keys" + data);
+        this.keys=data;
+        if(data!=null) {
+            this.key = data.get(index).key;
+            this.size = data.size();
+        }
     }
 
     public Stock getStockData(String stockName){
-        Stock stock =null;
-        final String uri = URI.key.replace("StockName",stockName).replace("StockKey",key);
-        try {
-            stock = objectMapper.readValue(restTemplate.getForObject(uri, String.class), Stock.class);
-        }catch (Exception e){
-            log.info("External API Failed");
-            e.printStackTrace();
+        int start = 0;
+        Stock stock = null;
+        while(start < size) {
+            final String uri = URI.key.replace("StockName", stockName).replace("StockKey", key);
+            log.info(uri);
+            try {
+                stock = objectMapper.readValue(restTemplate.getForObject(uri, String.class), Stock.class);
+                index = (index + 1) % size;
+                key = keys.get(index).key;
+                start = start + 1;
+                break;
+            } catch (Exception e) {
+                log.info("External API Failed Key Switch");
+                index = (index + 1) % size;
+                key = keys.get(index).key;
+                start = start + 1;
+            }
         }
         return stock;
     }
