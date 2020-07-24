467a99ba2c11681cb9a5ce1eb70b16c8df7b21ee
vashishthamishra <utkarsh.mishra30@gmail.com>
2020-07-24 15:54:32 -0700

    Added Config.
    Signed-off-by: vashishthamishra <utkarsh.mishra30@gmail.com>

---
+++ StockTradeSimluation/stock-user-service/src/main/java/com/stockuserservice/Config/Config.java
@@ -0,0 +1,18 @@
+package com.stockuserservice.Config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class Config {
+
+    @Bean
+    ObjectMapper objectMapper(){
+        return  new ObjectMapper();
+    }
+
+    @Bean
+    RestTemplate restTemplate(){
+        return  new RestTemplate();
+    }
+}
